f3ff66cbbb70e363c5699bea2b3d9865d0a17e6e
Stanislav Pivovartsev <stanislav_pivovartsev@mail.ru>
2015-08-05 22:29:37 +0300

    Test add source

--- src/AppBundle/Model/Source.php
+++ src/AppBundle/Model/Source.php
@@ -13,6 +13,8 @@ class Source
     const SOURCE_TYPE_RSS = 1;
     const SOURCE_TYPE_FACEBOOK = 2;
 
+    const FB_ACCESS_TOKEN = 'CAACFb9bXwZBUBAEidei6U0xFTDOKttjIFx5j3DRiF2iI8o2VvLAWhjR0psv2XC6VVG8ZCASiP7EVNTXkoSBXAo2cx1E8FQhYOiHx8PixlLKnxuY4mxk7p5tt2ld1dpEfZAYOM1oru0kSFhncROl7bKNuNrcl3agY7A7dJzrP6wVSPvCCOGzAy1wZAgbv6QYapiSCfVHlE7WY2F6LNRhV';
+
     /**
      * @var EntityManager
      */
@@ -55,6 +57,15 @@ class Source
         if (is_null($userId)) {
             $userId = App::getCurrentUserId();
         }
+
+        if ($type == self::SOURCE_TYPE_FACEBOOK) {
+            if (preg_match('/\//', $source)) {
+                return false;
+            } else {
+                $source = 'https://graph.facebook.com/fg.gov.ua/posts?access_token=' . self::FB_ACCESS_TOKEN;
+            }
+        }
+
 
         $entity = new SourceEntity();
         $entity->setSource($source);
--- src/AppBundle/Tests/Model/Source.php
+++ src/AppBundle/Tests/Model/Source.php
@@ -34,22 +34,55 @@ class SourceTest extends KernelTestCase
         $entity = $this->em->getRepository('AppBundle:FeedSource')->findOneBy(['source' => $rssSource]);
         $this->assertNotNull($entity);
 
-        $this->em->remove($entity);
-        $this->em->flush();
+        $this->removeSourceEntity($rssSource);
     }
 
-    public function testAddFacebook()
+    public function testFailAddFacebook()
     {
         $source = new Source();
         $source->setEm($this->em);
-        $facebookSource = 'https://graph.facebook.com/fg.gov.ua/posts?access_token=' . App::FB_ACCESS_TOKEN;
-        $this->assertTrue($source->add($facebookSource, Source::SOURCE_TYPE_FACEBOOK));
+        $facebookSource = 'https://graph.facebook.com/fg.gov.ua/posts?access_token=' . Source::FB_ACCESS_TOKEN;
+        $this->removeSourceEntity($facebookSource);
+        $this->assertFalse($source->add($facebookSource, Source::SOURCE_TYPE_FACEBOOK));
 
         $entity = $this->em->getRepository('AppBundle:FeedSource')->findOneBy(['source' => $facebookSource]);
-        $this->assertNotNull($entity);
+        $this->assertNull($entity);
+    }
 
-        $this->em->remove($entity);
-        $this->em->flush();
+    public function testSuccessAddFacebook()
+    {
+        $source = new Source();
+        $source->setEm($this->em);
+        $facebookSource = 'fg.gov.ua';
+        $this->assertTrue($source->add($facebookSource, Source::SOURCE_TYPE_FACEBOOK));
+
+        $fullSource = 'https://graph.facebook.com/' . $facebookSource . '/posts?access_token=' . Source::FB_ACCESS_TOKEN;
+
+        /** @var SourceEntity $entity */
+        $entity = $this->em->getRepository('AppBundle:FeedSource')->findOneBy(['source' => $fullSource]);
+
+        $partOfFullSource = 'https://graph.facebook.com/fg.gov.ua/posts?access_token=';
+        $result = !is_null($entity) && strpos($entity->getSource(), $partOfFullSource) !== false;
+
+        if (!is_null($entity)) {
+            $this->em->remove($entity);
+            $this->em->flush();
+        }
+
+        $this->assertTrue($result, $entity->getSource() . ' <===> ' . $partOfFullSource);
+    }
+
+    /**
+     * @param $source
+     */
+    private function removeSourceEntity($source)
+    {
+        $entity = $this->em->getRepository('AppBundle:FeedSource')->findOneBy(['source' => $source]);
+
+        if (!is_null($entity)) {
+            $this->em->remove($entity);
+            $this->em->flush();
+        }
     }
 
 
